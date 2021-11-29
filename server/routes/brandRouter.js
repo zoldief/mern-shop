127693cd84c8ea510c58d8705614a0e66416830d
Anton Kashchyk <dgtronv@gmail.com>
2021-11-29 23:09:14 +0300

    fix: fix tabs

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,8 +1,8 @@
-const Router = require('express')
-const router = new Router()
-const brandController = require('../controllers/brandController')
+const Router = require('express');
+const router = new Router();
+const brandController = require('../controllers/brandController');
 
-router.post('/', brandController.create)
-router.get('/', brandController.getAll)
+router.post('/', brandController.create);
+router.get('/', brandController.getAll);
 
-module.exports = router
+module.exports = router;
